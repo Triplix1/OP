5a719d50ea5d6305681cc3a885e1992ed0485381
Vlad <vladislav@gmail.com>
2021-12-22 11:17:22 +0200

    Update Lab_9_Python.py

--- Lab 9 Python/Lab_9_Python.py
+++ Lab 9 Python/Lab_9_Python.py
@@ -2,7 +2,7 @@ def funk (str, k):
     i = 0
     number = 0
     while i < len(str):
-        number = (int)(str. find(" ", i)) if (int)(str.find(" ",i)) > 0 else len(str)
+        number = str. find(" ", i) if str.find(" ",i) > 0 else len(str)
         if (number - i < k):
             print(str[i: number])
         i = number + 1;
